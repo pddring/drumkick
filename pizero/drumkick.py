7fe69d8b3c55b2ce7fbba199b1b751be2e710b1d
Pete Dring <pddring@gmail.com>
2024-12-20 15:10:18 +0000

    Now scans for midi hardware every 5s

--- pizero/drumkick.py
+++ pizero/drumkick.py
@@ -4,6 +4,7 @@ import mido
 import time
 import serial
 import os
+import threading
 
 print("""drumkick.py - Detect which button has been pressed and play a midi note
 
@@ -45,10 +46,13 @@ def handle_button(pin):
     else:
        vel = pin
 
+    if vel > 127:
+      vel = 127
+
     if midi_out == '':
       os.system("aplay samples/bass.wav &")
     else:
-    	midi_out.send(mido.Message('note_on', note=36, channel=9, velocity=vel))
+      midi_out.send(mido.Message('note_on', note=36, channel=9, velocity=vel))
     print("Playing bass drum with velocty {}".format(vel))
 
 
@@ -59,27 +63,50 @@ for pin in BUTTONS:
     GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
 
 # Connect to midi devices
-output_devices = mido.get_output_names()
-allowed_devices = ['MPK mini Play mk3:MPK mini Play mk3 MIDI 1 24:0', 'USBMIDI:USBMIDI USBMIDI 24:0']
-output_device_name = ""
-for dev in output_devices:
-  print("Found midi output device: ", dev)
-  if dev in allowed_devices:
-    print("Found MIDI device:", dev)
-    output_device_name = dev
-print("Connecting to MIDI output:", output_device_name)
-if output_device_name == "":
-  print("Sorry - no compatable MIDI output devices detected: debug mode only")
-  midi_out = ""
-else:
-  midi_out = mido.open_output(output_device_name)
+
+def detect_midi():
+  global midi_out
+  global output_device_name
+  output_devices = mido.get_output_names()
+  allowed_devices = ['MPK mini Play', 'USBMIDI', 'TD-17']
+  output_device_name = ""
+  for dev in output_devices:
+    print("Found midi output device: ", dev)
+    for allowed in allowed_devices:
+      if allowed in dev:
+        print("Found MIDI device:", dev)
+        output_device_name = dev
+  print("Connecting to MIDI output:", output_device_name)
+  if output_device_name == "":
+    print("Sorry - no compatable MIDI output devices detected: debug mode only")
+    midi_out = ""
+  else:
+    midi_out = mido.open_output(output_device_name)
+
+detect_midi()
 
 print("Connecting to serial port")
 trigger = serial.Serial("/dev/ttyUSB0", 115200)
 
+print("Loaded successfully: playing cymbal crash sound as indicator")
+os.system("aplay samples/ride_edge.wav&")
+
+
+# check if any devices are disconnected
+def detect_usb_changes():
+
+  while True:
+    print("Checking USB devices")
+    detect_midi()
+    time.sleep(10)
+
+
+
 # Finally, since button handlers don't require a "while True" loop,
 # we pause the script to prevent it exiting immediately.
+t_check_usb = threading.Thread(target = detect_usb_changes)
+t_check_usb.start()
+
 while True:
   volume = int(trigger.readline().decode('ascii').strip())
   handle_button(volume)
-  print(volume)
