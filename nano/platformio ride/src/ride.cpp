b8ff01bd9f89cd1cd8f089da21f5d6eb39d06c30
Pete Dring <dringp@fulford.york.sch.uk>
2025-01-11 09:03:38 +0000

    Update ride.cpp

--- nano/platformio ride/src/ride.cpp
+++ nano/platformio ride/src/ride.cpp
@@ -24,8 +24,12 @@ const int DELAY = 2;
 const int NOTE_ON_TIME = 150;
 const int STARTUP_FLASH_TIME = 50;
 const int STARTUP_FLASH_COUNT = 5;
-const int FILTER_SIZE = 50;
-const int SCALE_DIVIDER = 6;
+const int FILTER_SIZE = 10;
+const int PRE_SCALE_DIVIDER = 4;
+const int POST_SCALE_DIVIDER_BELL = 10;
+const int POST_SCALE_DIVIDER_BOW = 8;
+const int POST_SCALE_DIVIDER_EDGE = 10;
+const unsigned int IDLE_THRESHOLD = 15;
 
 // rolling filters
 int filterBell[FILTER_SIZE];
@@ -35,9 +39,13 @@ int filterEdge[FILTER_SIZE];
 int iBell = 0;
 int iBow = 0;
 int iEdge = 0;
-int totalBell;
-int totalBow;
-int totalEdge;
+long totalBell;
+long totalBow;
+long totalEdge;
+
+unsigned int idleBell = 0;
+unsigned int idleBow = 0;
+unsigned int idleEdge = 0;
 
 
 void setup() {
@@ -53,47 +61,52 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the bow of the ride:
-  valueBow = analogRead(pinBow);
-  if(valueBow > THRESHOLD) {
-    filterBow[iBow] = valueBow;
-    iBow++;
-  }
-
-  // read the value from the edge of the ride:
-  valueEdge = analogRead(pinEdge);
-  if(valueEdge > THRESHOLD) {
-    filterEdge[iEdge] = valueEdge;
-    iEdge++;
-  }
-
-  // read the value from the bow of the ride:
-  valueBell = analogRead(pinBell);
-  if(valueBell > THRESHOLD) {
-    filterBell[iBell] = valueBell;
-    iBell++;
-  }
-
   if(iBow == FILTER_SIZE || iBell == FILTER_SIZE || iEdge == FILTER_SIZE) {
     for(iBow; iBow > 0; iBow--) {
-      totalBow += filterBow[iBow];
+      totalBow += filterBow[iBow] / PRE_SCALE_DIVIDER;
     }
     for(iEdge; iEdge > 0; iEdge--) {
-      totalEdge += filterEdge[iEdge];
+      totalEdge += filterEdge[iEdge] / PRE_SCALE_DIVIDER;
     }
     for(iBell; iBell > 0; iBell--) {
-      totalBell += filterBell[iBell];
+      totalBell += filterBell[iBell] / PRE_SCALE_DIVIDER;
     }  
-    valueBow = totalBow / SCALE_DIVIDER;
-    valueEdge = totalEdge / SCALE_DIVIDER;
-    valueBell = totalBell / SCALE_DIVIDER;
     
-    Serial.print("BOW:");
+    /*Serial.print("BOW:");
     Serial.print(totalBow);
     Serial.print(" EDGE:");
     Serial.print(totalEdge);
     Serial.print(" BELL:");
-    Serial.println(totalBell);
+    Serial.print(totalBell);
+    
+    Serial.print(" IDLE_BELL: ");
+    Serial.print(idleBell);
+    Serial.print(" IDLE_EDGE: ");
+    Serial.print(idleEdge);
+    Serial.print(" IDLE_BOW: ");
+    Serial.print(idleBow);
+    */
+
+    unsigned int totalIdle = idleEdge + idleBow + idleBell;
+    if(totalIdle > IDLE_THRESHOLD) {
+      // Serial.println(" > IGNORE");
+    } else {
+      if(totalEdge > totalBow && totalEdge > totalBell) {
+        Serial.print("EDGE:");
+        totalEdge = totalEdge / POST_SCALE_DIVIDER_EDGE;    
+        Serial.println(totalEdge);
+      }
+      if(totalBow >= totalEdge && totalBow >= totalBell) {
+        Serial.print("BOW:");
+        totalBow = totalBow / POST_SCALE_DIVIDER_BOW;
+        Serial.println(totalBow);
+      }
+      if(totalBell > totalBow && totalBell > totalEdge) {
+        Serial.print("BELL:");
+        totalBell = totalBell / POST_SCALE_DIVIDER_BELL;
+        Serial.println(totalBell);
+      }
+    }
 
     totalBow = 0;
     totalEdge = 0;
@@ -105,7 +118,39 @@ void loop() {
     delay(NOTE_ON_TIME);
     // turn the ledPin off:
     digitalWrite(ledPin, LOW);
-  } 
+  }
+
+  // read the value from the bow of the ride:
+  valueBow = analogRead(pinBow);
+  if(valueBow > THRESHOLD) {
+    filterBow[iBow] = valueBow;
+    iBow++;
+    idleBow = 0;
+  } else {
+    idleBow++;
+  }
+
+  // read the value from the edge of the ride:
+  valueEdge = analogRead(pinEdge);
+  if(valueEdge > THRESHOLD) {
+    filterEdge[iEdge] = valueEdge;
+    iEdge++;
+    idleEdge = 0;
+  } else {
+    idleEdge++;
+  }
+
+  // read the value from the bow of the ride:
+  valueBell = analogRead(pinBell);
+  if(valueBell > THRESHOLD) {
+    filterBell[iBell] = valueBell;
+    iBell++;
+    idleBell = 0;
+  } else {
+    idleBell++;
+  }
+
+   
 }
 
 
