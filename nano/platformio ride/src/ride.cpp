79e1f284323d12b5c62aa0bff5755491e83f6f66
Pete Dring <dringp@fulford.york.sch.uk>
2025-01-19 08:51:11 +0000

    Update ride.cpp
    Sent all three readings in one line so that sensitivity can be done on the pi

--- nano/platformio ride/src/ride.cpp
+++ nano/platformio ride/src/ride.cpp
@@ -1,57 +1,60 @@
 /*
-  Piezo
+  Ride
 
-  Reads the value from a piezo transducer connected to A0. 
+  Reads the value from a three piezo transducers connected to A0, A1 and A2 
   
   Flashes the built in LED when the piezo is hit and constantly sends the sensor reading via serial port.
+  Range of values sent will be between 10 and 1023
+  Whichever piezo detects the largest signal sends one of the following lines:
+    BOW: 1023
+    EDGE: 1023
+    BELL: 1023
+
+  There should be 2x10K resistors forming a potential divider between GND and 5v to bias it to 2.5v (or 512)
+  The piezo should be connected to the middle of the potential divider and pin A0
+  There should be a 1M resistor in parallel with the piezos
 */
+
+typedef enum {
+  STATE_IDLE, STATE_RISING, STATE_FALLING
+} states;
+
 #include <Arduino.h>
 
-// pins
-int pinBell = A1;               // Piezo connected to bell of the ride
-int pinBow = A0;                // Piezo connected to bow of the ride
-int pinEdge = A2;               // Piezo connected to edge of the ride
-int ledPin = LED_BUILTIN;       // select the pin for the LED
+const int SENSORS = 3;                        // number of piezo sensors
+const char * SENSOR_NAMES[SENSORS] = {"BOW", "BELL", "EDGE"};
 
-// values read from each piezo
-int valueBell = 0;            
-int valueBow = 0;
-int valueEdge = 0;
+int sensorPin[SENSORS] = {A0, A1, A2};        // pin connected to the piezo transducer
+int ledPin = LED_BUILTIN;                     // select the pin for the built in LED
+int sensorValue[SENSORS] = {0, 0, 0};         // stores the absolute sensor value (will be biased to around 512)
 
-// properties
-const int THRESHOLD = 10;
-const int DELAY = 2;
-const int NOTE_ON_TIME = 40;
-const int STARTUP_FLASH_TIME = 50;
-const int STARTUP_FLASH_COUNT = 5;
-const int FILTER_SIZE = 50;
-const int PRE_SCALE_DIVIDER = 4;
-const int POST_SCALE_DIVIDER_BELL = 32;
-const int POST_SCALE_DIVIDER_BOW = 24;
-const int POST_SCALE_DIVIDER_EDGE = 32;
-const unsigned int IDLE_THRESHOLD = 15;
+const int STARTUP_FLASH_TIME = 50;            // flash speed on startup
+const int STARTUP_FLASH_COUNT = 5;            // number of times the LED flashes on startup
 
-// rolling filters
-int filterBell[FILTER_SIZE];
-int filterBow[FILTER_SIZE];
-int filterEdge[FILTER_SIZE];
+const int THRESHOLD_RISING = 60;              // threshold to cross before moving out of idle into rising
+const int THRESHOLD_FALLING = 20;             // threshold ot cross before moving out of falling back into idle
 
-int iBell = 0;
-int iBow = 0;
-int iEdge = 0;
-long totalBell;
-long totalBow;
-long totalEdge;
+const int SAMPLE_FILTER_SIZE = 50;            // number of samples used to calculate the range
+int sampleFilter[SENSORS][SAMPLE_FILTER_SIZE];// used to store the raw sample values (rolling window filter)
+int iSample[SENSORS];                         // index of the current sample
 
-unsigned int idleBell = 0;
-unsigned int idleBow = 0;
-unsigned int idleEdge = 0;
-unsigned int totalIdle = 0;
-
+int previousRange[SENSORS] = {0, 0, 0};       // used to detect if the amplitude is rising or falling
+states state[SENSORS] = {                     // state machine starts falling (initial range will be 512, decreasing to 0)
+  STATE_FALLING,
+  STATE_FALLING,
+  STATE_FALLING
+};        
 
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
+  
+  // set up piezo pins as INPUT
+  for(int i = 0; i < SENSORS; i++) {
+    pinMode(sensorPin[i], INPUT);
+  }
+
+  // flash LED on startup
   for(int i = 0; i < STARTUP_FLASH_COUNT; i++) {
     digitalWrite(ledPin, HIGH);
     delay(STARTUP_FLASH_TIME);
@@ -62,93 +65,78 @@ void setup() {
 }
 
 void loop() {
-  if(iBow == FILTER_SIZE || iBell == FILTER_SIZE || iEdge == FILTER_SIZE) {
-    for(iBow; iBow > 0; iBow--) {
-      totalBow += filterBow[iBow] / PRE_SCALE_DIVIDER;
+  for(int iPiezo = 0; iPiezo < SENSORS; iPiezo++) {
+    // read the value from the sensor:
+    sensorValue[iPiezo] = analogRead(sensorPin[iPiezo]);
+    sampleFilter[iPiezo][iSample[iPiezo]] = sensorValue[iPiezo];
+    iSample[iPiezo]++;
+    if(iSample[iPiezo] == SAMPLE_FILTER_SIZE) {
+      iSample[iPiezo] = 0;
     }
-    totalBow /= POST_SCALE_DIVIDER_BOW;
     
-    for(iEdge; iEdge > 0; iEdge--) {
-      totalEdge += filterEdge[iEdge] / PRE_SCALE_DIVIDER;
-    }
-    totalEdge /= POST_SCALE_DIVIDER_EDGE;
-
-    for(iBell; iBell > 0; iBell--) {
-      totalBell += filterBell[iBell] / PRE_SCALE_DIVIDER;
-    }  
-    totalBell /= POST_SCALE_DIVIDER_BELL;
-    
-    if(totalEdge > totalBow && totalEdge > totalBell) {
-      totalIdle = idleBow + idleBell;
-      if(totalIdle > IDLE_THRESHOLD) {
-        // Serial.println("Ignore Edge");
-      } else {
-        Serial.print("EDGE:");
-        Serial.println(totalEdge);
+    // calculate min, max and range of all recorded samples in the window filter
+    int min = sampleFilter[iPiezo][0];
+    int max = sampleFilter[iPiezo][0];
+    for(int i = 1; i < SAMPLE_FILTER_SIZE; i++) {
+      if(sampleFilter[iPiezo][i] < min) {
+        min = sampleFilter[iPiezo][i];
+      }
+      if(sampleFilter[iPiezo][i] > max) {
+        max = sampleFilter[iPiezo][i];
       }
     }
-    if(totalBow >= totalEdge && totalBow >= totalBell) {
-      totalIdle = idleEdge + idleBell;
-      if(totalIdle > IDLE_THRESHOLD) {
-        // Serial.println("Ignore Bow");
-      } else {
-        Serial.print("BOW:");
-        Serial.println(totalBow);
-      }
+    int range = max - min;
+
+    /* Debugging info: State, sample, range */
+    /*
+    Serial.print(state);
+    Serial.print(" ");
+    Serial.print(sensorValue);
+    Serial.print(" ");
+    Serial.println(range);
+    */
+
+    switch(state[iPiezo]) {
+      // ignore low level changes (noise) until a spike is detected
+      case STATE_IDLE:
+        if(range > THRESHOLD_RISING) {
+          state[iPiezo] = STATE_RISING;
+          previousRange[iPiezo] = range;
+        }
+        break;
+
+      // spike detected - need to wait until the peak is reached
+      case STATE_RISING:
+        if(range < previousRange[iPiezo]) {
+          digitalWrite(ledPin, HIGH);
+          
+          // determine which piezo has the highest value
+          int max = 0;
+          int iMax = 0;
+          for(int i = 0; i < SENSORS; i++) {
+            Serial.print(SENSOR_NAMES[i]);
+            Serial.print(":");
+            Serial.print(previousRange[i]);
+            Serial.print(" ");
+            if(previousRange[i] > max) {
+              max = previousRange[i];
+              iMax = i;
+            }            
+            state[i] = STATE_FALLING;
+          }
+          Serial.println();
+        } else {
+          previousRange[iPiezo] = range;
+        }
+        break;
+      
+      // peak detected - need to ignore values until level falls low enough to start looking for new spike
+      case STATE_FALLING:
+        if(range < THRESHOLD_FALLING) {
+          state[iPiezo] = STATE_IDLE;
+          digitalWrite(ledPin, LOW);
+        }
+        break;
     }
-    if(totalBell > totalBow && totalBell > totalEdge) {
-      totalIdle = idleEdge + idleBow;
-      if(totalIdle > IDLE_THRESHOLD) {
-        // Serial.println("Ignore Bell");
-      } else {
-        Serial.print("BELL:");
-        Serial.println(totalBell);
-      }
-    }
-  
-    totalBow = 0;
-    totalEdge = 0;
-    totalBell = 0;
-
-    // turn the ledPin on
-    digitalWrite(ledPin, HIGH);
-    // stop the program for <sensorValue> milliseconds:
-    delay(NOTE_ON_TIME);
-    // turn the ledPin off:
-    digitalWrite(ledPin, LOW);
   }
-
-  // read the value from the bow of the ride:
-  valueBow = analogRead(pinBow);
-  filterBow[iBow] = valueBow;
-  iBow++;
-  if(valueBow > THRESHOLD) {
-    idleBow = 0;
-  } else {
-    idleBow++;
-  }
-
-  // read the value from the edge of the ride:
-  valueEdge = analogRead(pinEdge);
-  filterEdge[iEdge] = valueEdge;
-  iEdge++;
-  if(valueEdge > THRESHOLD) {  
-    idleEdge = 0;
-  } else {
-    idleEdge++;
-  }
-
-  // read the value from the bow of the ride:
-  valueBell = analogRead(pinBell);
-  filterBell[iBell] = valueBell;
-  iBell++;
-  if(valueBell > THRESHOLD) {
-    idleBell = 0;
-  } else {
-    idleBell++;
-  }
-
-   
 }
-
-
